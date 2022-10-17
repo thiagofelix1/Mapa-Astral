3bc0ceb0439a87ec6f8e08a11c265a4cf3f702a7
thiagofelix1 <thiagofelixdasilva099@gmail.com>
2022-10-17 13:58:02 -0400

    aplicacao

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,17 @@
-package org.example;public class Main {
+package org.example;
+
+import java.time.*;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LocalDateTime dataNascimento = LocalDateTime.of(2000,12,11,5,0);
+        Pessoa pessoa = new Pessoa(dataNascimento);
+        System.out.println("idade: " + pessoa.getIdade());
+        System.out.println("data nascimento: " + pessoa.getDataNascimentoFormatada());
+        System.out.println("Zoneoffset: " + pessoa.getZoneOffSet());
+        System.out.println("Ano de nascimento bissexto:  " + pessoa.isAnoBissexto());
+        System.out.println("signo: " + pessoa.getSigno());
+        System.out.println("ascedente: " + pessoa.getAscendente());
+        System.out.println("signo lunar: " + pessoa.getSignoLunar());
     }
 }
--- src/main/java/org/example/Pessoa.java
+++ src/main/java/org/example/Pessoa.java
@@ -1,4 +1,93 @@
 package org.example;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+
 public class Pessoa {
+    private final LocalDateTime dataNascimento;
+
+    private final MonthDay aniversario;
+
+    private final ZonedDateTime zonedDateTime;
+
+    public Pessoa(LocalDateTime dataNascimento) {
+        this.dataNascimento = dataNascimento;
+        aniversario = MonthDay.of(dataNascimento.getMonth(), dataNascimento.getDayOfMonth());
+        zonedDateTime = ZonedDateTime.of(dataNascimento, ZoneId.of("America/Manaus"));
+    }
+
+    public Integer getIdade() {
+        return Period.between(dataNascimento.toLocalDate(), LocalDate.now()).getYears();
+    }
+
+    public String getDataNascimentoFormatada() {
+        return dataNascimento.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+    }
+
+    public ZoneOffset getZoneOffSet() {
+        return zonedDateTime.toOffsetDateTime().getOffset();
+    }
+
+    public Boolean isAnoBissexto() {
+        return Year.isLeap(dataNascimento.getYear());
+    }
+
+    public String getSigno() {
+        MonthDay leaoComecaEm = MonthDay.of(7,22);
+        MonthDay leaoTerminaEm = MonthDay.of(8,23);
+
+        MonthDay sagitarioComecaEm = MonthDay.of(11,21);
+        MonthDay sagitarioTerminaEm = MonthDay.of(12,22);
+
+        MonthDay aquarioComecaEm = MonthDay.of(01,21);
+        MonthDay aquarioTerminaEm = MonthDay.of(02,19);
+
+        MonthDay cancerComecaEm = MonthDay.of(06,22);
+        MonthDay cancerTerminaEm = MonthDay.of(07,22);
+
+        MonthDay escorpiaoComecaEm = MonthDay.of(10,23);		MonthDay escorpiaoTerminaEm = MonthDay.of(11,21);
+
+        if (verificarSeEstaEntreDatas(aniversario, leaoComecaEm, leaoTerminaEm)) return "Leão";
+
+        if (verificarSeEstaEntreDatas(aniversario, sagitarioComecaEm, sagitarioTerminaEm)) return "Sagitário";
+
+        if (verificarSeEstaEntreDatas(aniversario, aquarioComecaEm, aquarioTerminaEm)) return "Aquaporin";
+
+        if (verificarSeEstaEntreDatas(aniversario, escorpiaoComecaEm, escorpiaoTerminaEm)) return "Escorpião";
+
+        if (verificarSeEstaEntreDatas(aniversario, cancerComecaEm, cancerTerminaEm)) return "Câncer";
+
+        return null;
+    }
+
+    public String getAscendente() {
+        if ("Leão".equalsIgnoreCase(getSigno())) {
+            if (verificarSeEstaEntreHorarios(LocalTime.of(dataNascimento.getHour(), dataNascimento.getMinute()), LocalTime.of(18, 0), LocalTime.of(19, 59))) return "Áries";
+        }
+        if ("Sagitário".equalsIgnoreCase(getSigno())) {
+            if (verificarSeEstaEntreHorarios(LocalTime.of(dataNascimento.getHour(), dataNascimento.getMinute()), LocalTime.of(12, 0), LocalTime.of(13, 59))) return "Peixes";
+        }
+        return "Ascendente não encontrado";
+    }
+
+    private static boolean verificarSeEstaEntreHorarios(LocalTime horaParaVerificar, LocalTime horaInicio, LocalTime horaFim) {
+        return !(horaParaVerificar.isBefore(horaInicio) || horaParaVerificar.isAfter(horaFim)) ;
+    }
+
+    public String getSignoLunar() {
+        if (zonedDateTime.getZone() == ZoneId.of("America/Recife") && dataNascimento.getHour() > 12) {
+            return "Casimiro";
+        }
+        if (zonedDateTime.getZone() == ZoneId.of("America/Cuiaba") && dataNascimento.getHour() < 12) {
+            return "Odin";
+        }
+        if (zonedDateTime.getZone() == ZoneId.of("America/Sao_Paulo")) {
+            return "Gandalf";
+        }
+        return "Dinossauro";
+    }
+
+    private static boolean verificarSeEstaEntreDatas(MonthDay dataParaVerificar, MonthDay dataInicio, MonthDay dataFim) {
+        return !(dataParaVerificar.isBefore(dataInicio) || dataParaVerificar.isAfter(dataFim)) ;
+    }
 }
